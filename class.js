98c5f3039fe92d2441da31593eaee8b0f4a1d000
sajib689 <jibonraja11@gmail.com>
2024-02-23 10:35:21 +0600

    class

---
+++ class.js
@@ -0,0 +1,12 @@
+const prducts = [
+    {id: 1, name: 'lenvo', price: 65000},
+    {id: 2, name: 'dell', price: 45000},
+    {id: 3, name: 'hp', price: 47000},
+    {id: 4, name: 'mac', price: 150000},
+]
+
+class Product {
+    speak(talk){
+        console.log(`talking about ${talk}`)
+    }
+}
